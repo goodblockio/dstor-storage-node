cb5f54f8cec1df053917e5dd0471de0cc0515545
pmbadger <pm@steepshot.io>
2022-07-21 21:31:43 +0300

    fix: typo

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const OutpostHttpServer = require('./src/OutpostHttpServer')
 const OutpostWorker = require('./src/OutpostWorker')
 
 const worker = new OutpostWorker()
-const httpServer = new OutpostHttpServer()
+const httpServer = new OutpostHttpServer(worker)
 
 worker.start()
 httpServer.start()
