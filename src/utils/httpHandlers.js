8cbd105ddc59baad5f4319e9337ac6eb027d5263
pmbadger <pm@steepshot.io>
2023-01-31 19:12:55 +0300

    feat: add server timing for node

--- src/utils/httpHandlers.js
+++ src/utils/httpHandlers.js
@@ -1,12 +1,21 @@
 const { STANDART_HASH_SIZE } = require("./constants")
 const {performance} = require("perf_hooks");
+const {createServerTiming} = require("./CreateServerTiming");
 
-const replyFromIPFS = (reply, requestPathTail, hash) => {
+const replyFromIPFS = (reply, requestPathTail, hash, serverTiming, serverTimingFlag) => {
   try {
     const tail = requestPathTail ? `/${requestPathTail}` : ''
     const redirectUrl = `${process.env.IPFS_GATEWAY_URL}/ipfs/${hash}${tail}`
+    if(serverTimingFlag){
+      serverTiming.push({ t: performance.now(), d: "Url redirect" })
+      reply.header('Server-Timing', `${createServerTiming(serverTiming)}`)
+    }
     return reply.from(redirectUrl)
   } catch (e) {
+    if(serverTimingFlag){
+      serverTiming.push({ t: performance.now(), d: "Url redirect error" })
+      reply.header('Server-Timing', `${createServerTiming(serverTiming)}`)
+    }
     return reply.code(500).send('An error occurred while fetching contents by hash')
   }
 }
@@ -20,7 +29,7 @@ const getObjByHashHandler = async (httpServer, request, reply, serverTimingFlag 
   if(serverTimingFlag) serverTiming.push({ t: performance.now(), d: "Start Time" })
 
   if (hashFromParams.length === STANDART_HASH_SIZE) {
-    return replyFromIPFS(reply, requestPathTail, hashFromParams)
+    return replyFromIPFS(reply, requestPathTail, hashFromParams, serverTiming, serverTimingFlag)
   }
 
   let availableUntilISO, hash, key
@@ -30,17 +39,28 @@ const getObjByHashHandler = async (httpServer, request, reply, serverTimingFlag 
     hash = decryptedHashData[1]
     key = decryptedHashData[2]
   } catch (e) {
+    if(serverTimingFlag){
+      serverTiming.push({ t: performance.now(), d: "Decrypted hash error" })
+      reply.header('Server-Timing', `${createServerTiming(serverTiming)}`)
+    }
     return reply.code(400).send('Unable to decrypt a hash')
   }
+  if(serverTimingFlag) serverTiming.push({ t: performance.now(), d: "Decrypted hash" })
     
   const dateAvailableUntil = new Date(availableUntilISO), currentDate = new Date()
   if (dateAvailableUntil < currentDate) {
+    if(serverTimingFlag){
+      reply.header('Server-Timing', `${createServerTiming(serverTiming)}`)
+    }
     return reply.code(400).send('Signature expired. Please access a file/folder again using our api server')
   }
+  if(serverTimingFlag) serverTiming.push({ t: performance.now(), d: "Signature Expiration Check" })
 
   httpServer.outpostWorker.sendObjectAccessedMessage(key, requestPathTail)
 
-  return replyFromIPFS(reply, requestPathTail, hash)
+  if(serverTimingFlag) serverTiming.push({ t: performance.now(), d: "Sending a message about object access" })
+
+  return replyFromIPFS(reply, requestPathTail, hash, serverTiming, serverTimingFlag)
 }
 
 const handleNoHashRedirectHandler = async (request, reply) => {
